8dbfffc8d3684e3a798800900b7be8cfdfea54ad
MURALI34CR7 <muraliprasathk.2005@gmail.com>
2025-04-12 05:04:07 +0530

    changes10

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY']='East'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///TravelBuddy.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-
+citys=["Chennai","Bengaluru","Hyderabad","Odisha"]
 db.init_app(app)
 with app.app_context():
     db.create_all()
 
-city_id=[1,2,3]
-cities={"chennai":{1:"Fairlands",2:"Marketplace",3:"3 roads",4:"4 roads",5:"old bus stand",6:"new bustand"}}
+cities={"Chennai":{1:"Fairlands",2:"Marketplace",3:"3 roads",4:"4 roads",5:"Old bus stand",6:"New bustand"},
+        "Bengaluru":{1:"Bengaluru palace",2:"Cubbon park",3:"UB city",4:"Phoneix marketcity",5:"V.V.puram",6:"Muthyalamaduvu"},
+        "Hyderabad":{1:"Charminar",2:"Jubilee hills",3:"HITEC city",4:"Shaikpet",5:"Secundarnad",6:"Vijayawada"},
+        "Odisha":{1:"Konarak",2:"Bhubhaneswar",3:"Cuttack",4:"gangai",5:"Jajapur",6:"Ganjam"}}
 @app.route('/')
 def home():
     return redirect(url_for('login'))
@@ -49,9 +51,9 @@ def banner(cur_log):
     return render_template('banner.html',cur_log=cur_log,data=cities)
 
 
-@app.route('/communitylobby')
-def communitylobby():
-    return render_template("communitylobby.html",data=cities["chennai"])
+@app.route('/<int:cur_log>/<int:city>communitylobby')
+def communitylobby(cur_log,city):
+    return render_template("communitylobby.html",cur_log=cur_log,city=city,data=cities[citys[city-1]],cit=citys[city-1])
 
 
 @app.route("/group_chat")
